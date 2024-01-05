ea326ea2f4ec81555528955cfec1ca24b02c83f4
A Rajaguhan <sparrowblack437@gmail.com>
2024-01-05 06:34:06 +0530

    modified:   Rent.py

--- Rent.py
+++ Rent.py
@@ -2,6 +2,17 @@
 from ODBC import ODBC
 from Vehicle import Vehicle
 
+from datetime import datetime
+import os
+
+import rich
+from rich.console import Console
+from rich.table import Table
+from rich import print as rprint
+from rich import box
+
+import time
+
 class Rent(ODBC, Vehicle):
     
     def book_rentals(self):
@@ -17,9 +28,29 @@ class Rent(ODBC, Vehicle):
                 break
             print()
             veh_type = veh_chk[0]
-            deposit = input('Pay Deposit ( Minimum to pay is 10,000 ) | Q to back ')
-            if deposit.lower() == 'q':
-                break
-            if deposit<10000:
-                print('Minimum Deposit is 10,000, Amount transacted Back')
+            
+            x2 = 1
+            while x2:
+                
+                rprint('Minimum to pay is 10,000 ')
+                rprint('Maximum to pay is 30,000 ')
+
+                deposit = input('Pay Deposit | Q to back ')
+                if deposit.lower() == 'q':
+                    break
+                elif int(deposit)<10000:
+                    rprint('Minimum Deposit is 10,000, Amount transacted Back')
+                    rprint('Pay More than minimum to rent Vehicle')
+                elif int(deposit)>30000:
+                    rprint('Maximum Deposit is 30,000, Amount transacted Back')
+                    rprint('Pay less than maximum to rent Vehicle')
+                elif 10000<=int(deposit)<=30000:
+                    rprint('Deposited Accepted')
+                    x2 = 0
+                else:
+                    rprint('Invalid | Pay Again !!! ')
+                    continue
+                
+                
+                    
     
